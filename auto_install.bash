9a66bb7f071469b6e3f1ea00d2d58ff897379f1a
gtirth <109450810+gtirth@users.noreply.github.com>
2023-07-25 14:51:42 -0400

    updated zerotier installation

--- auto_install.bash
+++ auto_install.bash
@@ -81,8 +81,11 @@ source ~/.bashrc
 
 echo "installing Zerotier"
 sleep 5
-curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && \
-if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
+#curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && \
+#if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
+curl -s https://install.zerotier.com | sudo bash
+
+
 
 echo "make sure to reboot the computer once this script has run succesfully"
 #can-bus rules 
